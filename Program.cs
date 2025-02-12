b20ff3f19d00f3b02c64b07b24020a9f3abb8e17
INovitski <INovitski@hazeltree.com>
2025-02-12 13:12:47 +0000

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+using System.Text;
+
+using var input = Console.OpenStandardInput();
+using var output = Console.OpenStandardOutput();
+using var writer = new XmlTextWriter(output, Encoding.Unicode);
+var document = new XmlDocument();
+
+document.Load(input);
+writer.Formatting = Formatting.Indented;
+
+document.WriteContentTo(writer);
