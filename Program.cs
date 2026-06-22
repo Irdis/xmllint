a3f74f8ef6ea9e751980bbafe44bdb218e685c8f
INovitski <INovitski@hazeltree.com>
2026-06-22 13:26:23 +0100

    add some defaults

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Text;
 
 using var input = Console.OpenStandardInput();
 using var output = Console.OpenStandardOutput();
-using var writer = new XmlTextWriter(output, Encoding.Unicode);
+using var writer = new XmlTextWriter(output, new UTF8Encoding(
+        encoderShouldEmitUTF8Identifier: false
+));
 var document = new XmlDocument();
 
 document.Load(input);
